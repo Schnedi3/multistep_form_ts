1bc949d13d176ff13fdfee4e33f350412a180f46
Schnedi3 <schnedi3@gmail.com>
2024-09-01 00:45:07 +0200

    Step 4: Implement "summary" logic

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -1,9 +1,20 @@
 import { NavLink } from "react-router-dom";
 
 import { summary } from "../data";
+import { useFormContext } from "../context/useFormContext";
+
 import "../css/summary.css";
+import "../css/common.css";
 
 export const Summary = () => {
+  const { planData, addonData, billing } = useFormContext();
+
+  const planCost = billing === "Yearly" ? planData.year : planData.month;
+  const addonsCost = addonData.reduce((total, addon) => {
+    return total + (billing === "Yearly" ? addon.year : addon.month);
+  }, 0);
+  const totalCost = planCost + addonsCost;
+
   return (
     <>
       <section className="card_container">
@@ -15,40 +26,37 @@ export const Summary = () => {
         <article className="summary">
           <div className="summary_plan">
             <div>
-              <h4>Arcade (Monthly)</h4>
+              <h4>
+                {planData.title} ({billing})
+              </h4>
               <NavLink to="/plan">Change</NavLink>
             </div>
-            <h3>$9/mo</h3>
+            <h3>${billing === "Yearly" ? planData.year : planData.month}</h3>
           </div>
 
-          <div className="summary_addon">
-            <div>
-              <p>Online service</p>
-              <h3>+$1/mo</h3>
+          {addonData.map((addon) => (
+            <div className="summary_addon">
+              <p>{addon.title}</p>
+              <h3>+${billing === "Yearly" ? addon.year : addon.month}</h3>
             </div>
-            <div>
-              <p>Larger storage</p>
-              <h3>+$2/mo</h3>
-            </div>
-            <div>
-              <p>Customizable profile</p>
-              <h3>+$2/mo</h3>
-            </div>
-          </div>
+          ))}
         </article>
 
         <article className="total">
-          <p>Total (per month)</p>
-          <h3>+$12/mo</h3>
+          <p>Total ({billing === "Yearly" ? "per year" : "per month"})</p>
+          <h3>
+            ${totalCost}
+            {billing === "Yearly" ? "/yr" : "/mo"}
+          </h3>
         </article>
       </section>
 
-      <footer className="finish_footer">
-        <button className="finish_previous">
+      <footer className="footer">
+        <button className="footer_previous">
           <NavLink to="/addons">Go Back</NavLink>
         </button>
 
-        <button className="finish_next">
+        <button className="footer_next">
           <NavLink to="/thanks">Confirm</NavLink>
         </button>
       </footer>
